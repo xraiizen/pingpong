fee00f02c58761c93277555f837dcb728b4ec2b7
e.morvan <e.morvan@inanix.fr>
2024-03-15 17:40:09 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
 const socket = io("http://127.0.0.1:3000/");
 const canvas = document.getElementById('ballCanvas');
 const ctx = canvas.getContext('2d');
+
+// Initialiser le canvas
+window.addEventListener('resize', setupCanvas);
+socket.on('role', (role) => {
+    userRole = role;
+    ball.visible = role === 'hôte';
+    setupCanvas();
+});
+
+// // Mettre à jour la position de la balle en fonction des données reçues
+socket.on('updateBallPosition', (data) => {
+    ball.x = data.x;
+    ball.dx = data.dx;
+    ball.dy = data.dy;
+    ball.velocity = data.velocity;
+    ball.visible = data.visible;
+
+    if (ball.visible && ball.y === canvas.height / 2) {
+        ball.y = -ball.radius; // Commencer juste au-dessus du haut de l'écran
+    }
+});
+
+// Mettre à jour les scores en fonction des données reçues
+socket.on('updateScore', (updatedScore) => {
+    score.host = updatedScore.host;
+    score.client = updatedScore.client;
+    drawScores();
+});
+
 let decalageY = 15;
 let ball = {
     radius: 10,
-    x: 0,
-    y: 0,
+    x: null,
+    y: null,
     dx: 0,
     dy: 0,
-    visible: false,
-    velocity: 3
+    visible: true,
+    velocity: 1
 };
+
 let paddle = {
     height: 10,
     width: 75,
@@ -30,32 +60,6 @@ let score = {
 };
 let userRole = '';
 
-function setupCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight + decalageY;
-    animateSetup();
-}
-
-
-function animateSetup() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    drawGoals();
-    drawScores();
-    collisionRaquette();
-    collisionBords();
-    requestAnimationFrame(animateSetup);
-}
-
-function resetBallPosition() {
-    ball.x = canvas.width / 2 - (ball.radius / 2);
-    ball.y = canvas.height / 2 - (ball.radius / 2);
-    ball.dx = 0;
-    ball.dy = 0;
-    ball.velocity = 3;
-}
 
 function drawBall() {
     if (!ball.visible) return;
@@ -83,9 +87,9 @@ function drawPaddle() {
 }
 
 function drawGoals() {
-    goal.x= canvas.width / 2 - goal.width / 2,
-    goal.y= canvas.height - goal.height,
-    ctx.beginPath();
+    goal.x = canvas.width / 2 - goal.width / 2,
+        goal.y = canvas.height - goal.height,
+        ctx.beginPath();
     ctx.rect(goal.x, goal.y, goal.width, goal.height);
     ctx.fillStyle = "#FFD700";
     ctx.fill();
@@ -103,96 +107,101 @@ function drawScores() {
         75); // Positionner le score du client en haut à droite
 }
 
-//     function updateBallPosition() {
-//       if (!ball.visible) return;
-//       // Démarrez le mouvement de la balle lors de la première collision avec la raquette
-//       //console.log(ball.dy, ball.dx, ball.y, paddle.y - ball.radius);
-//       if (ball.dy === 0 && ball.dx === 0 && ball.y === paddle.y - ball.radius) {
-//         console.log(ball.dy, ball.dx, ball.y, paddle.y - ball.radius)
-//         if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-//           ball.velocity = 1; // Réinitialisez la vitesse si nécessaire
-//           ball.dy = -2 * ball.velocity; // Définissez les valeurs initiales pour commencer le mouvement
-//           ball.dx = 8 * ((ball.x - (paddle.x + paddle.width / 2)) / paddle.width);
-//         }
-//       }
+function setupCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight + decalageY;
+    // Initialise ou réinitialise la position de la balle au centre après le redimensionnement
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    animateSetup();
+}
 
-//       // Collision avec les bords latéraux du canvas
-//       if (ball.x + ball.dx > canvas.width - ball.radius || ball.x + ball.dx < ball.radius) {
-//         ball.dx = -ball.dx;
-//       }
 
-//       // Collision avec la raquette, en prenant en compte la position verticale actuelle de la balle
-//       if (ball.dy > 0 && ball.y < paddle.y - ball.radius && ball.y + ball.dy >= paddle.y - ball.radius) {
-//         if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-//           ball.dy = -ball.dy * ball.velocity; // Inverse la direction verticale de la balle et applique l'accélération
-//           ball.dx = 8 * ((ball.x - (paddle.x + paddle.width / 2)) / paddle
-//           .width); // Applique l'accélération horizontale
-//           // Pas besoin de réajuster `ball.y` ici car l'ajustement est basé sur la condition de collision
-//         }
-//       }
+function animateSetup() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    drawGoals();
+    drawScores();
+    collisionRaquette();
+    collisionBords();
+    requestAnimationFrame(animateSetup);
+}
 
-//       // Gère la collision avec le haut du canvas ou la transmission de la balle
-//       if (ball.y < 0) {
-//           ball.visible = false;
-//           sendData();
-//           return; // Arrête la mise à jour de la position pour éviter les mouvements inattendus
+function resetBallPosition() {
+    ball.x = canvas.width / 2 - (ball.radius / 2);
+    ball.y = canvas.height / 2 - (ball.radius / 2);
+    ball.dx = 0;
+    ball.dy = 0;
+    ball.velocity = 1;
+}
 
-//       }
+function UpdateBallToOtherScreen() {
+    ball.x = canvas.width / 2; // Centrer la balle sur l'axe des x
+    ball.y = ball.radius + 5; // Placer la balle un peu en dessous du haut du canvas pour éviter qu'elle ne soit immédiatement considérée comme sortie
+    ball.dx = 0; // Réinitialiser le déplacement horizontal
+    ball.dy = 0; // Réinitialiser le déplacement vertical
+    ball.velocity = 1; // Réinitialiser la vitesse de la balle
+}
 
-//       // Rebond sur le bas du canvas
-//       if (ball.y + ball.dy > canvas.height - ball.radius) {
-//     // Supposons que le but inférieur appartient à l'hôte
-//     if (ball.x > canvas.width / 2 - goal.width / 2 && ball.x < canvas.width / 2 + goal.width / 2) {
-//         if (userRole === 'hôte') {
-//             score.client++;
-//         } else if (userRole === 'client') {
-//             score.host++;
-//         }
-//         resetBallPosition();
-//         sendData();
-//     } else {
-//         ball.dy = -ball.dy;
+// function collisionRaquette() {
+//     // Calculer les côtés de la raquette
+//     let paddleTop = paddle.y;
+//     let paddleBottom = paddle.y + paddle.height;
+//     let paddleLeft = paddle.x;
+//     let paddleRight = paddle.x + paddle.width;
+
+//     // Calculer le point le plus proche sur la raquette à la balle
+//     let closestPointX = Math.max(paddleLeft, Math.min(ball.x, paddleRight));
+//     let closestPointY = Math.max(paddleTop, Math.min(ball.y, paddleBottom));
+
+//     // Calculer la distance entre le point le plus proche sur la raquette et le centre de la balle
+//     let distanceX = ball.x - closestPointX;
+//     let distanceY = ball.y - closestPointY;
+
+//     // Calculer la distance en utilisant le théorème de Pythagore
+//     let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+//     // Vérifier si la distance est inférieure au rayon de la balle (collision détectée)
+//     if (distance <= ball.radius) {
+//         // Inverser la direction verticale de la balle
+
+//         ball.dy = ball.velocity * ((ball.y - (paddle.y + paddle.width / 2)) / paddle.width);
+//         ball.dx = ball.velocity * ((ball.x - (paddle.x + paddle.width / 2)) / paddle.width);
+
+
+
+//         console.log("Collision détectée");
+//         console.log("ball.dx,", ball.dx, "ball.dy", ball.dy);
 //     }
+//     // Mettre à jour la position de la balle
+//     ball.x += ball.dx;
+//     ball.y += ball.dy;
 // }
-
-//       ball.x += ball.dx;
-//       ball.y += ball.dy;
-//     }
-
 function collisionRaquette() {
-    // Calculer les côtés de la raquette
-    let paddleTop = paddle.y;
-    let paddleBottom = paddle.y + paddle.height;
-    let paddleLeft = paddle.x;
-    let paddleRight = paddle.x + paddle.width;
+    // Vérifier si la balle est au niveau de la raquette
+    if (ball.y + ball.radius >= paddle.y && ball.y - ball.radius <= paddle.y + paddle.height) {
+        if (ball.x >= paddle.x && ball.x <= paddle.x + paddle.width) {
+            // Inverser la direction verticale de la balle
+            ball.dy = -ball.dy;
 
-    // Calculer le point le plus proche sur la raquette à la balle
-    let closestPointX = Math.max(paddleLeft, Math.min(ball.x, paddleRight));
-    let closestPointY = Math.max(paddleTop, Math.min(ball.y, paddleBottom));
+            // Ajuster le dy en fonction de l'endroit où la balle touche la raquette horizontalement
+            let touchPointX = (ball.y - (paddle.y + paddle.width / 2)) / (paddle.width / 2);
+            // Ici, vous pourriez vouloir ajuster la vitesse verticale dy basée sur touchPointX si c'était votre intention originale
+            // Par exemple, si vous souhaitez que le contact au centre de la raquette envoie la balle verticalement vers le haut avec plus de force
+            ball.dy -= touchPointX * ball.velocity +2; // Cela réduira ou augmentera dy basé sur où la balle touche la raquette
 
-    // Calculer la distance entre le point le plus proche sur la raquette et le centre de la balle
-    let distanceX = ball.x - closestPointX;
-    let distanceY = ball.y - closestPointY;
-
-    // Calculer la distance en utilisant le théorème de Pythagore
-    let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
-    // Vérifier si la distance est inférieure au rayon de la balle (collision détectée)
-    if (distance <= ball.radius) {
-        // Inverser la direction verticale de la balle
-
-        ball.dy = ball.velocity * ((ball.y - (paddle.y + paddle.width / 2)) / paddle.width);
-        ball.dx = ball.velocity * ((ball.x - (paddle.x + paddle.width / 2)) / paddle.width);
-
-
-
-        console.log("Collision détectée");
-        console.log("ball.dx,", ball.dx, "ball.dy", ball.dy);
+            // Assurez-vous que la balle ne "traverse" pas la raquette en ajustant sa position y
+            ball.y = paddle.y - ball.radius;
+        }
     }
-    // Mettre à jour la position de la balle
+    // Mise à jour de la position de la balle
     ball.x += ball.dx;
     ball.y += ball.dy;
 }
+
+
 
 function collisionBords() {
     // Rebond sur les bords latéraux
@@ -202,10 +211,14 @@ function collisionBords() {
     }
 
     //  Rebond dans le goals
-   
+
     if (ball.x + ball.radius >= goal.x && ball.x - ball.radius <= goal.x + goal.width && ball.y + ball.radius >= canvas.height - goal.height) {
         console.log("rebond dans le but");
-        ball.dy = -ball.dy;
+        if (userRole === 'hôte') {
+            score.client++;
+        } else if (userRole === 'client') {
+            score.host++;
+        }
         resetBallPosition();
         sendData();
     }
@@ -221,7 +234,7 @@ function collisionBords() {
         console.log("ball.y - ball.radius", ball.y - ball.radius)
         ball.visible = false;
         sendData();
-        resetBallPosition();
+        UpdateBallToOtherScreen();
     }
 
     // Mettre à jour la position de la balle si elle est visible
@@ -231,17 +244,13 @@ function collisionBords() {
     }
 }
 
-
-
-
-
 function sendData() {
     socket.emit('updateBallPosition', {
         x: ball.x,
- 
         dx: ball.dx,
         dy: -ball.dy,
-        velocity: ball.velocity
+        velocity: ball.velocity,
+        visible: true
     });
     socket.emit('updateScore', {
         host: score.host,
@@ -249,11 +258,7 @@ function sendData() {
     });
 }
 
-
-
-
 document.addEventListener("mousemove", mouseMoveHandler, false);
-
 
 // Gérer le mouvement de la raquette
 function mouseMoveHandler(e) {
@@ -266,26 +271,3 @@ function mouseMoveHandler(e) {
         paddle.y = relativeY - paddle.height / 2;
     }
 }
-
-// Initialiser le canvas
-window.addEventListener('resize', setupCanvas);
-socket.on('role', (role) => {
-    userRole = role;
-    setupCanvas();
-});
-
-// Mettre à jour la position de la balle en fonction des données reçues
-socket.on('updateBallPosition', (data) => {
-    ball.x = data.x;
-    ball.dx = data.dx;
-    ball.dy = data.dy;
-    ball.velocity = data.velocity;
-    ball.visible = true;
-});
-
-// Mettre à jour les scores en fonction des données reçues
-socket.on('updateScore', (updatedScore) => {
-    score.host = updatedScore.host;
-    score.client = updatedScore.client;
-    drawScores();
-});
